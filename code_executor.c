dd2f0165753c96b46d2713fba7fc41fa69192011
Axel <afratoni@itba.edu.ar>
2017-11-30 20:29:27 -0300

    Agregadas todas las funcionalidades de las operaciones

--- code_executor.c
+++ code_executor.c
@@ -9,21 +9,33 @@ extern Board board;
 extern char* code;
 extern char* function[BUFFER_SIZE];
 extern struct point board_size;
+extern int tags [BUFFER_SIZE];
+struct point op_numbers;
 int function_pointer;
 int bucket[BUFFER_SIZE] = {0};
 int bucket_pointer;
+int pos_i;
+int pos_j;
+int board_length;
+int board_width;
 
 void run_code(){
   bucket_pointer = 0;
   function_pointer = 0;
-  int pos_i = 0;
-  int pos_j = 0;
-  int code_pointer = 0; // Nombre mdio choto, no le queria poner i
-  int len = strlen(code);
-  int board_length = board_size.b;
-  int board_width = board_size.a;
-  while(code_pointer < len){
-    switch(code[code_pointer]){
+  pos_i = 0;
+  pos_j = 0;
+  board_length = board_size.b;
+  board_width = board_size.a;
+  fill_functions();
+  fill_tags();
+  run(code);
+}
+
+void run(char* actual_code) {
+  //fprintf(stderr, "HOLA\n");
+  for(int code_pointer = 0; actual_code[code_pointer] != 0; code_pointer++){
+    //fprintf(stderr, "%c", actual_code[code_pointer]);
+    switch(actual_code[code_pointer]){
       case 'R':
           pos_j++;
           break;
@@ -31,25 +43,24 @@ void run_code(){
           pos_j--;
           break;
       case 'U':
-          pos_i++;
-          break;
-      case 'D':
           pos_i--;
           break;
+      case 'D':
+          pos_i++;
+          break;
       case 'J':
-          execute(pos_i,pos_j);
+          execute(pos_i,pos_j, actual_code, &code_pointer);
           break;
       }
-    if (pos_i< 0 || pos_i >= board_length || pos_j < 0 || pos_j >= board_width){
-      fprintf(stderr, "La cagaste perro, te fuiste de los limites\n");
+    if (pos_i< 0 || pos_i >= board_width || pos_j < 0 || pos_j >= board_length){
+      fprintf(stderr, "Te fuiste de los limites\n");
       exit(0);
     }
-
-    code_pointer++;
   }
 }
 
-void execute(int pos_i, int pos_j){
+void execute(int pos_i, int pos_j, char* actual_code, int *code_pointer){
+  //fprintf(stderr, "%d ", board[pos_i][pos_j].op_type);
   switch(board[pos_i][pos_j].op_type){
     case ipb:
         bucket_pointer++; 
@@ -64,36 +75,65 @@ void execute(int pos_i, int pos_j){
         function_pointer--;
         break;
     case rdc:
-        //readchar
+        bucket[bucket_pointer] = getchar();
         break;
     case ptc:
-        //putchar
+        putchar(bucket[bucket_pointer]);
         break;
     case ply:
-        //execute current function
+        run(function[function_pointer]);
         break;
     case ifz:
-        //ifz jump to tag
+        if(actual_code[*code_pointer + 1] != '{') {
+          fprintf(stderr, "No se encontro un goto\n");
+          exit(0);
+        }
+        int tag_num = 0;
+        int i;
+        for(i = 2; actual_code[*code_pointer + i] != '}'; i++){
+          tag_num = tag_num * 10 + (actual_code[*code_pointer + i] - '0');
+        }
+        if (bucket[bucket_pointer] != 0) {
+          *code_pointer = tags[tag_num - 1] - 1;
+        } else {
+          *code_pointer += i;
+        }
         break;
     case add:
-        //add
+        extract_numeric_op(pos_i, pos_j);
+        bucket[bucket_pointer] = op_numbers.a + op_numbers.b;
         break;
     case sub:
-        //sub
+        extract_numeric_op(pos_i, pos_j);
+        bucket[bucket_pointer] = op_numbers.a - op_numbers.b;
         break;
     case mul:
-        //mul
+        extract_numeric_op(pos_i, pos_j);
+        bucket[bucket_pointer] = op_numbers.a * op_numbers.b;
         break;
     case diV:
-        //div
+        extract_numeric_op(pos_i, pos_j);
+        bucket[bucket_pointer] = op_numbers.a / op_numbers.b;
         break;
   }
 }
 
+void extract_numeric_op(int pos_i, int pos_j){
+  switch(board[pos_i][pos_j].subtype){
+    case BB:
+      op_numbers.a = bucket[board[pos_i][pos_j].bb.a];
+      op_numbers.b = bucket[board[pos_i][pos_j].bb.b];
+      break;
+    case B:
+      op_numbers.a = bucket[bucket_pointer];
+      op_numbers.b = bucket[board[pos_i][pos_j].b];
+      break;
+    case NUM:
+      op_numbers.a = bucket[bucket_pointer];
+      op_numbers.b = board[pos_i][pos_j].num;
+      break;
+    default:
 
-
-
-
-
-
-
+      break;
+  }
+}
--- code_executor.h
+++ code_executor.h
@@ -4,5 +4,9 @@
 #define BUFFER_SIZE 1024
 
 void run_code();
-void execute(int pos_i, int pos_j);
+void run(char* actual_code);
+void execute(int pos_i, int pos_j, char* actual_code, int *code_pointer);
+void extract_numeric_op(int pos_i, int pos_j);
+extern void fill_functions();
+extern void fill_tags();
 #endif
